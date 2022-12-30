02431debd53a5799990b8b949056a28773d240c7
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2022-12-30 13:39:27 -0600

    Chapter5exercises

--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -1,5 +1,7 @@
 package org.launchcode.java.demos.lsn4classes2;
 
+import java.util.Objects;
+
 public class Student {
 
     private static int nextStudentId = 1;
@@ -29,50 +31,82 @@ public class Student {
     }
 
 
-     //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
+    //TODO: Uncomment and complete the getGradeLevel method here:
+    public String getGradeLevel() {
+        if (this.numberOfCredits >= 0) {
+            return "Freshman";
+        } else if (this.numberOfCredits >= 30) {
+            return "Sophomore";
+        } else if (this.numberOfCredits >= 60) {
+            return "Junior";
+        } else if (this.numberOfCredits >= 90) {
+            return "Senior";
 //        // Determine the grade level of the student based on numberOfCredits
-//    }
+        }
+        return "invalid data";
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
         // Update the appropriate fields: numberOfCredits, gpa
+
+        double totalQualityScore = this.gpa * this.numberOfCredits;
+        totalQualityScore += courseCredits * grade;
+        this.numberOfCredits += courseCredits;
+
+        this.gpa = (totalQualityScore / this.numberOfCredits);
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", studentId=" + studentId +
+                ", numberOfCredits=" + numberOfCredits +
+                ", gpa=" + gpa +
+                '}';
+    }
+
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (studentId != student.studentId) return false;
+        if (numberOfCredits != student.numberOfCredits) return false;
+        if (Double.compare(student.gpa, gpa) != 0) return false;
+        return Objects.equals(name, student.name);
+    }
 
     public String getName() {
         return name;
     }
-
     public int getStudentId() {
         return studentId;
     }
-
     public int getNumberOfCredits() {
         return numberOfCredits;
     }
-
     public double getGpa() {
         return gpa;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public void setStudentId(int studentId) {
         this.studentId = studentId;
     }
-
     public void setGpa(double gpa) {
         this.gpa = gpa;
     }
-
     private void setNumberOfCredits(int numberOfCredits) {
         this.numberOfCredits = numberOfCredits;
     }
