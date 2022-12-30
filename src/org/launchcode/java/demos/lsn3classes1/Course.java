8d6bc260a937c02d5c63ef1dc4a9866050e85cfa
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2022-12-30 10:42:57 -0600

    Chapter4

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+
+public class Course {
+    private String courseName;
+    private int numberOfCredits;
+    private Teacher teacherName;
+    private ArrayList<Student> enrolledStudents;
+}
--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,18 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student mattWicks = new Student("Matt Wicks", 1234, 1, 4.0);
+
+        System.out.println(mattWicks);
+
     }
 }
+
+//    What access modifier restricts access the most for what we need?
+//    A:  question poorly worded, what is "for what we need" supposed to refer to?
+
+//    If it is a field, could we restrict the access to private and use getters and setters? -
+//    A:  yes.
+
+//    If we do set fields to private, what access level do we have to give our getters and setters?
+//    A:  public.
