eaec08848954b6ef17ae239722bd130767033d78
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-01-09 20:18:59 -0600

    studio3.8

--- src/org/launchcode/java/countingchars/Test.java
+++ src/org/launchcode/java/countingchars/Test.java
@@ -2,10 +2,24 @@ package org.launchcode.java.countingchars;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a phrase: ");
+        String str = input.nextLine().toLowerCase();
+
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            charCount.merge(c, 1, Integer::sum);
+        }
+        for (char key : charCount.keySet()) {
+            System.out.println(key + ": " + charCount.get(key));
+        }
+
+    /*
         String str = "this is a test String";
         Map<Character, Integer> charCount = new HashMap<>();
         for (char c : str.toCharArray()) {
@@ -17,6 +31,7 @@ public class Test {
 
         for (char key : charCount.keySet()) {
             System.out.println(key + ": " + charCount.get(key));
-        }
+
+     */
     }
 }
